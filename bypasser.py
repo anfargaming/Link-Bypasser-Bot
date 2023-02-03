8f99526b9ceb44dfd16b3b4018cbde499c0e9b46
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-02-03 18:03:27 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1139,35 +1139,38 @@ def adfly(url):
 ##############################################################################################        
 # gplinks
 
-url = "https://gplinks.co/VfA5O1"  #@param {type:"string"}
-
 def gplinks_bypass(url: str):
- 
     url = url[:-1] if url[-1] == '/' else url
     token = url.split("/")[-1]
-    
-    domain ="https://gplinks.co/"
+
+    domain = "https://gplinks.co/"
     referer = "https://mynewsmedia.co/"
 
-    
     client = requests.Session()
-    vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
-    url = f"{url}/?{vid}"
-
     response = client.get(url, allow_redirects=False)
-    soup = BeautifulSoup(response.content, "html.parser")
-    
-    
-    inputs = soup.find(id="go-link").find_all(name="input")
-    data = { input.get('name'): input.get('value') for input in inputs }
-    
+    if "Location" in response.headers:
+        vid = response.headers["Location"].split("=")[-1]
+        url = f"{url}/?{vid}"
 
-    time.sleep(5)
-    headers={"x-requested-with": "XMLHttpRequest"}
-    bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
-    return bypassed_url
+        response = client.get(url, allow_redirects=False)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-print(gplinks_bypass(url))
+        inputs = soup.find(id="go-link").find_all(name="input")
+        data = {input.get('name'): input.get('value') for input in inputs}
+
+        time.sleep(5)
+        headers = {"x-requested-with": "XMLHttpRequest"}
+        bypassed_url = client.post(domain + "links/go", data=data, headers=headers).json()["url"]
+        return bypassed_url
+    else:
+        return None
+
+url = "https://gplinks.co/your_url"
+bypassed_url = gplinks_bypass(url)
+if bypassed_url:
+    print(bypassed_url)
+else:
+    print("No bypassed URL found")
 
 
 ######################################################################################################
