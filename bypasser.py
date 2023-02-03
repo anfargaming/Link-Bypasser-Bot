1ba523cdc896f14f583ff932e8a41e8619bb0265
anfargaming <120932993+anfargaming@users.noreply.github.com>
2023-02-03 17:57:22 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1139,31 +1139,33 @@ def adfly(url):
 ##############################################################################################        
 # gplinks
 
+url = "https://gplinks.co/VfA5O1"  #@param {type:"string"}
+
 def gplinks_bypass(url: str):
  
- url = url[:-1] if url[-1] == '/' else url
- token = url.split("/")[-1]
+    url = url[:-1] if url[-1] == '/' else url
+    token = url.split("/")[-1]
     
- domain ="https://gplinks.co/"
- referer = "https://mynewsmedia.co/"
+    domain ="https://gplinks.co/"
+    referer = "https://mynewsmedia.co/"
 
     
- client = requests.Session()
- vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
- url = f"{url}/?{vid}"
+    client = requests.Session()
+    vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
+    url = f"{url}/?{vid}"
 
- response = client.get(url, allow_redirects=False)
- soup = BeautifulSoup(response.content, "html.parser")
+    response = client.get(url, allow_redirects=False)
+    soup = BeautifulSoup(response.content, "html.parser")
     
     
- inputs = soup.find(id="go-link").find_all(name="input")
- data = { input.get('name'): input.get('value') for input in inputs }
+    inputs = soup.find(id="go-link").find_all(name="input")
+    data = { input.get('name'): input.get('value') for input in inputs }
     
 
- time.sleep(5)
- headers={"x-requested-with": "XMLHttpRequest"}
- bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
- return bypassed_url
+    time.sleep(5)
+    headers={"x-requested-with": "XMLHttpRequest"}
+    bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
+    return bypassed_url
 
 print(gplinks_bypass(url))
 
